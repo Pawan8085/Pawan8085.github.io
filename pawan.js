b85e1d6b1e3d79d0ee4469dec4d0e8a4612ba9c4
Pawan <kumarpawanm8085@gmail.com>
2023-05-01 16:00:26 +0530

    java_project_added

--- pawan.js
+++ pawan.js
@@ -9,24 +9,24 @@ function func(){
         document.getElementById('tskill').style.color="#444444";
         document.getElementById('ttool').style.color="#444444";
         document.getElementById('prjtitle').style.color="#444444";
-        document.getElementById("hi").style.color="#444444";
+        document.getElementById("user-detail-name").style.color="#444444";
         document.getElementById("developer").style.color="#444444";
         document.getElementById("sskillheading").style.color="#444444";
         document.getElementById("staticheading").style.color="#444444";
-        document.getElementById('myheader').style.backgroundColor="#ffffff";
+        document.getElementById('nav-menu').style.backgroundColor="#ffffff";
         bool=false;
         
     }
     else{
         document.body.style.backgroundColor = " #23283e";
-        document.getElementById("hi").style.color="#cdcdff";
+        document.getElementById("user-detail-name").style.color="#cdcdff";
         document.getElementById("developer").style.color="#cdcdff";
         document.getElementById('tskill').style.color="#cdcdff";
         document.getElementById('ttool').style.color="#cdcdff";
         document.getElementById('prjtitle').style.color="#cdcdff";
         document.getElementById('sskillheading').style.color="#cdcdff";
         document.getElementById('staticheading').style.color="#cdcdff";
-        document.getElementById('myheader').style.backgroundColor="#23283e";
+        document.getElementById('nav-menu').style.backgroundColor="#23283e";
         bool=true;
        
     }
@@ -37,7 +37,7 @@ document.getElementById("resume").addEventListener('click',resume);
 function resume() {
     window.location.href="https://drive.google.com/file/d/1q-OXB0o1eUY9oYk5yeAfnyNVXXX2b5OV/view";
 }
-document.getElementById("git").addEventListener('click',git);
+document.getElementById("contact-github").addEventListener('click',git);
 function git() {
     // window.location.href="https://github.com/Pawan8085";
     window.open("https://github.com/Pawan8085",'_blank');
@@ -49,7 +49,7 @@ function linkedin() {
 }
 document.getElementById('d1').addEventListener('click', d1);
 function d1(){
-    let url='https://inquisitive-kashata-a201b3.netlify.app/index.html'; 
+    let url='https://github.com/Pawan8085/Ecommerce'; 
     window.open(url,'_blank');
 }
 
@@ -70,13 +70,13 @@ function d3(){
 document.getElementById('d4').addEventListener('click', d4);
 function d4(){
     
-   let url='https://resplendent-scone-4001e5.netlify.app/';
+   let url='https://github.com/Pawan8085/Email-Application';
    window.open(url,'_blank');
 }
 
 document.getElementById('s1').addEventListener('click', s1);
 function s1(){
-    let url='https://github.com/sumitraghavwork/bashful-form-6526';
+    let url='https://github.com/Pawan8085/Ecommerce';
     window.open(url,'_blank');
 }
 
@@ -94,7 +94,7 @@ function s3(){
 
 document.getElementById('s4').addEventListener('click', s4);
 function s4(){
-    let url='https://github.com/masai-course/Pawan_fw18_1190/tree/master/unit-3/sprint-4/evaluation/U3__c4_';
+    let url='https://github.com/Pawan8085/Email-Application';
     window.open(url,'_blank');
 }
 
